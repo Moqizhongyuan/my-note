34c7138fea2a3d941ff98fc30bbc0722f5a23828
Moqizhongyuan <2356373295@qq.com>
2024-10-29 23:40:46 +0800

    ✨新增(Next): 项目初始化
    初始化项目文档、数据库，显示了一些内容，设置了代码提交规范

---
+++ components/Sidebar.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { getAllNotes } from "@/lib/redis";
+import SidebarNoteList from "@/components/SidebarNoteList";
+
+export default async function Sidebar() {
+  const notes = await getAllNotes();
+  return (
+    <>
+      <section className="col sidebar">
+        <Link href={"/"} className="link--unstyled">
+          <section className="sidebar-header">
+            {/* <img
+              className="logo"
+              src="/logo.svg"
+              width="22px"
+              height="20px"
+              alt=""
+              role="presentation"
+            /> */}
+            <Image
+              className="logo"
+              src="/logo.svg"
+              width={100}
+              height={100}
+              alt=""
+              role="presentation"
+            />
+            <strong>React Notes</strong>
+          </section>
+        </Link>
+        <section className="sidebar-menu" role="menubar">
+          {/* SideSearchField */}
+        </section>
+        <nav>
+          <SidebarNoteList notes={notes} />
+        </nav>
+      </section>
+    </>
+  );
+}
